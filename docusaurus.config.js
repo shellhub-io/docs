3d136b33d2a995e785f6f96440c7ef5748554ebb
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2022-09-01 14:01:11 -0300

    config: update editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,14 +38,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/shellhub-io/docs/tree/master',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/shellhub-io/docs/tree/master',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
