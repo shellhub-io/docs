b40fd22e2e6fd61c6b6d228f86d7162c751c3fe5
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2024-01-31 14:59:47 -0300

    deps: bump docusaurus to 3.1.1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -114,8 +113,8 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} ShellHub.`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
       },
       algolia: {
         appId: 'U0BS9FJALJ',
